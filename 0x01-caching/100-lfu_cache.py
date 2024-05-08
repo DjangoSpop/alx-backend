9da8d2762134941134859af7854b915496720169
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-05-08 12:37:43 +0300

    foofoo

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,80 +1,55 @@
 #!/usr/bin/python3
+"""LFU Cache Replacement Implementation Class
 """
-wewill create a class that will
-cache useing python
-"""
+from threading import RLock
+
 BaseCaching = __import__('base_caching').BaseCaching
-from collections import defaultdict
-from datetime import datetime
+
 
 class LFUCache(BaseCaching):
     """
-    LFUCache class that inherits from BaseCaching.
+    An implementaion of LFUCache(Least frequently used)
 
-    Implements a Least Frequently Used (LFU) cache eviction policy.
+    Attributes:
+        __stats (list): A dictionary of cache keys for access count
+        __rlock (RLock): Lock accessed resources to prevent race condition
     """
-
     def __init__(self):
-        """Intialise the constructor"""
+        """ Instantiation method, sets instance attributes
+        """
         super().__init__()
-        self.frequency = defaultdict(int)
-        self.last_used = {}
+        self.__stats = {}
+        self.__rlock = RLock()
 
     def put(self, key, item):
+        """ Add an item in the cache
         """
-        Add an item to the cache.
-
-        Args:
-            key: The key of the item.
-            item: The item to be added.
-
-        Returns:
-            None
-        """
-        if key is None or item is None:
-            return
-
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            min_freq = min(self.frequency.values())
-            if len(self.cache_data) >= self.MAX_ITEMS:
-                # Find the LFU item(s)
-                min_frequency = min(self.frequency.values())
-                lfu_items = [k for k, v in self.frequency.items() if v == min_frequency]
-
-                # If more than one item has the minimum frequency, use LRU
-                if len(lfu_items) > 1:
-                    lfu_items.sort(key=lambda x: self.last_used[x])
-                
-                # Discard the LFU/LRU item
-                discard_key = lfu_items[0]
-                del self.cache_data[discard_key]
-                del self.frequency[discard_key]
-                del self.last_used[discard_key]
-                print(f"DISCARD: {discard_key}")
-
-            del self.cache_data[discard_key]
-            del self.frequency[discard_key]
-            del self.last_used[discard_key]
-            print(f"DISCARD: {discard_key}")
-
-        # Add the new item
-        self.cache_data[key] = item
-        self.frequency[key] += 1
-        self.last_used[key] = datetime.now()
+        if key is not None and item is not None:
+            keyOut = self._balance(key)
+            with self.__rlock:
+                self.cache_data.update({key: item})
+            if keyOut is not None:
+                print('DISCARD: {}'.format(keyOut))
 
     def get(self, key):
+        """ Get an item by key
         """
-        Retrieve an item from the cache.
+        with self.__rlock:
+            value = self.cache_data.get(key, None)
+            if key in self.__stats:
+                self.__stats[key] += 1
+        return value
 
-        Args:
-            key: The key of the item to retrieve.
-
-        Returns:
-            The item associated with the key, or None if the key is not found.
+    def _balance(self, keyIn):
+        """ Removes the earliest item from the cache at MAX size
         """
-        if key is None or key not in self.cache_data:
-            return None
-
-        self.frequency[key] += 1
-        self.last_used[key] = self.current_time
-        return self.cache_data[key]
+        keyOut = None
+        with self.__rlock:
+            if keyIn not in self.__stats:
+                if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                    keyOut = min(self.__stats, key=self.__stats.get)  # Specify the type of the key parameter
+                    self.cache_data.pop(keyOut)
+                    self.__stats.pop(keyOut)
+            self.__stats[keyIn] = self.__stats.get(keyIn, 0) + 1
+        return keyOut
+    
